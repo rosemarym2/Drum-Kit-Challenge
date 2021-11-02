d1d4a1543ce54327611bc5319d8c03cc33303cf2
rosemarym2 <rosemarymay91@gmail.com>
2021-11-02 17:28:38 +0000

    Drum Kit Challenge- based on template v1.3

--- script/script.js
+++ script/script.js
@@ -1,6 +1,48 @@
-const qKey = document.getElementById("qKey");
+const aKey = document.getElementById("aKey");
+const lKey = document.getElementById("lKey");
 
-qKey.addEventListener("click", () => {
+
+aKey.addEventListener("click", () => {
+    document.getElementById("boom").pause();
+    document.getElementById("boom").currentTime = 0;
+    document.getElementById("boom").play();
+})
+sKey.addEventListener("click", () => {
+    document.getElementById("clap").pause();
+    document.getElementById("clap").currentTime = 0;
+    document.getElementById("clap").play();
+})
+dKey.addEventListener("click", () => {
+    document.getElementById("hihat").pause();
+    document.getElementById("hihat").currentTime = 0;
+    document.getElementById("hihat").play();
+})
+fKey.addEventListener("click", () => {
+    document.getElementById("kick").pause();
+    document.getElementById("kick").currentTime = 0;
+    document.getElementById("kick").play();
+})
+gKey.addEventListener("click", () => {
+    document.getElementById("openhat").pause();
+    document.getElementById("openhat").currentTime = 0;
+    document.getElementById("openhat").play();
+})
+hKey.addEventListener("click", () => {
+    document.getElementById("ride").pause();
+    document.getElementById("ride").currentTime = 0;
+    document.getElementById("ride").play();
+})
+jKey.addEventListener("click", () => {
+    document.getElementById("snare").pause();
+    document.getElementById("snare").currentTime = 0;
+    document.getElementById("snare").play();
+})
+kKey.addEventListener("click", () => {
+    document.getElementById("tink").pause();
+    document.getElementById("tink").currentTime = 0;
+    document.getElementById("tink").play();
+})
+lKey.addEventListener("click", () => {
     document.getElementById("tom").pause();
     document.getElementById("tom").currentTime = 0;
     document.getElementById("tom").play();
