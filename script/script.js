a53fb0fc786355e038f0c444cc8bed819fb146be
rosemarym2 <rosemarymay91@gmail.com>
2021-11-03 13:38:19 +0000

    Drum Kit Challenge- based on template v1.4

--- script/script.js
+++ script/script.js
@@ -9,6 +9,21 @@ const kKey = document.getElementById("kKey");
 const lKey = document.getElementById("lKey");
 
 
+/*broken code-const keys = [`a`,`s`,`d`,`f`,`g`,`h`,`j`,`k`,`l`];
+const drumSounds = [`boom`,`clap`,`hihat`,`kick`,`openhat`,`ride`,`snare`,`tink`,`tom`];
+
+    document.addEventListener(`keydown`, (event) => {
+        for (let i=0; i < drumSounds.length; i++) {
+        if (keys.includes(event.key)){
+            let sounds = drumSounds[keys.indexOf(event.key)];
+            document.getElementById(`<p>${drumSounds}</p>`).pause();
+            document.getElementById(`<p>${drumSounds}</p>`).currentTime = 0;
+            document.getElementById(`<p>${drumSounds}</p>`).play();   
+        }
+    }
+    });*/
+
+
 aKey.addEventListener("click", () => {
     document.getElementById("boom").pause();
     document.getElementById("boom").currentTime = 0;
@@ -54,3 +69,49 @@ lKey.addEventListener("click", () => {
     document.getElementById("tom").currentTime = 0;
     document.getElementById("tom").play();
 })
+document.addEventListener("keydown", (event) => {
+    if (event.key == "a") {
+        document.getElementById("boom").pause();
+        document.getElementById("boom").currentTime = 0;
+        document.getElementById("boom").play();
+    } else if (event.key == "s") {
+        document.getElementById("clap").pause();
+        document.getElementById("clap").currentTime = 0;
+        document.getElementById("clap").play();
+    } else if (event.key == "d") {
+        document.getElementById("hihat").pause();
+        document.getElementById("hihat").currentTime = 0;
+        document.getElementById("hihat").play();
+    } else if (event.key == "f") {
+        document.getElementById("kick").pause();
+        document.getElementById("kick").currentTime = 0;
+        document.getElementById("kick").play();
+    } else if (event.key == "g") {
+        document.getElementById("openhat").pause();
+        document.getElementById("openhat").currentTime = 0;
+        document.getElementById("openhat").play();
+    } else if (event.key == "h") {
+        document.getElementById("ride").pause();
+        document.getElementById("ride").currentTime = 0;
+        document.getElementById("ride").play();
+    }else if (event.key == "j") {
+        document.getElementById("snare").pause();
+        document.getElementById("snare").currentTime = 0;
+        document.getElementById("snare").play();
+    }else if (event.key == "k") {
+        document.getElementById("tink").pause();
+        document.getElementById("tink").currentTime = 0;
+        document.getElementById("tink").play();
+    }else if (event.key == "l") {
+        document.getElementById("tom").pause();
+        document.getElementById("tom").currentTime = 0;
+        document.getElementById("tom").play();
+    }
+}); 
+
+/*const findp =(key)
+
+document.addEventListener ("keydown", (event)=>{
+    let p = findp(event.key);
+    if (p) p.classList.add("highlight");
+});*/
